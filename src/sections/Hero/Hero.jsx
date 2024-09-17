9ab0f91621c5728c914d06595437266c7ab97a34
chawki <achouriichawkii@gmail.com>
2024-09-18 00:24:52 +0100

    presque fini ^^

--- src/sections/Hero/Hero.jsx
+++ src/sections/Hero/Hero.jsx
@@ -14,7 +14,6 @@ import { useTheme } from '../../common/ThemeContext';
 
 function Hero() {
   const {theme, toggleTheme} = useTheme();
-
   const themeIcon = theme === 'light' ? sun : moon; 
   const twitterIcon = theme === 'light' ? twitterLight : twitterDark; 
   const githubIcon = theme === 'light' ? githubLight : githubDark; 
