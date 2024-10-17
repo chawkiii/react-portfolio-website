492cb3e0d31592ababd1d05a9110eb4df7fafb77
chawki <achouriichawkii@gmail.com>
2024-10-17 19:16:35 +0100

    cv

--- src/sections/Hero/Hero.jsx
+++ src/sections/Hero/Hero.jsx
@@ -1,62 +1,65 @@
-import styles from './HeroStyles.module.css';
-import heroImg from '../../assets/hero-img.png';
-import sun from '../../assets/sun.svg';
-import moon from '../../assets/moon.svg';
-import twitterLight from '../../assets/twitter-light.svg';
-import twitterDark from '../../assets/twitter-dark.svg';
-import githubLight from '../../assets/github-light.svg';
-import githubDark from '../../assets/github-dark.svg';
-import linkedinLight from '../../assets/linkedin-light.svg';
-import linkedinDark from '../../assets/linkedin-dark.svg';
-import cv from '../../assets/cv.pdf';
-import { useTheme } from '../../common/ThemeContext';
-
+import styles from "./HeroStyles.module.css";
+import heroImg from "../../assets/hero-img.png";
+import sun from "../../assets/sun.svg";
+import moon from "../../assets/moon.svg";
+import twitterLight from "../../assets/twitter-light.svg";
+import twitterDark from "../../assets/twitter-dark.svg";
+import githubLight from "../../assets/github-light.svg";
+import githubDark from "../../assets/github-dark.svg";
+import linkedinLight from "../../assets/linkedin-light.svg";
+import linkedinDark from "../../assets/linkedin-dark.svg";
+import { useTheme } from "../../common/ThemeContext";
 
 function Hero() {
-  const {theme, toggleTheme} = useTheme();
-  const themeIcon = theme === 'light' ? sun : moon; 
-  const twitterIcon = theme === 'light' ? twitterLight : twitterDark; 
-  const githubIcon = theme === 'light' ? githubLight : githubDark; 
-  const linkedinIcon = theme === 'light' ? linkedinLight : linkedinDark; 
+  const { theme, toggleTheme } = useTheme();
+  const themeIcon = theme === "light" ? sun : moon;
+  const twitterIcon = theme === "light" ? twitterLight : twitterDark;
+  const githubIcon = theme === "light" ? githubLight : githubDark;
+  const linkedinIcon = theme === "light" ? linkedinLight : linkedinDark;
 
   return (
-    <section id='hero' className={styles.container}>
+    <section id="hero" className={styles.container}>
       <div className={styles.colorModeContainer}>
-        <img 
+        <img
           className={styles.hero}
           src={heroImg}
-          alt="profile picture of chawki achouri" 
+          alt="profile picture of chawki achouri"
         />
-        <img 
-          className={styles.colorMode} 
-          src={themeIcon} 
+        <img
+          className={styles.colorMode}
+          src={themeIcon}
           alt="color mode icon"
-          onClick={toggleTheme} 
+          onClick={toggleTheme}
         />
       </div>
       <div className={styles.info}>
-        <h1>Achouri<br />Chawki</h1>
+        <h1>
+          Achouri
+          <br />
+          Chawki
+        </h1>
         <h2>Frontend Developer</h2>
         <span>
-          <a href="https://twitter.com" target='_blank'>
+          <a href="https://twitter.com" target="_blank">
             <img src={twitterIcon} alt="twitter icon" />
           </a>
-          <a href="https://github.com" target='_blank'>
+          <a href="https://github.com" target="_blank">
             <img src={githubIcon} alt="github icon" />
           </a>
-          <a href="https://linkedin.com" target='_blank'>
+          <a href="https://linkedin.com" target="_blank">
             <img src={linkedinIcon} alt="linkedin icon" />
           </a>
         </span>
         <p className={styles.description}>
-          with a passion for developping modern web apps for commercial businesses. 
+          with a passion for developping modern web apps for commercial
+          businesses.
         </p>
-        <a href={cv} download>
-          <button className='hover'>Resume</button>
+        <a href="https://chawkii-cv.netlify.app/">
+          <button className="hover">Resume</button>
         </a>
       </div>
     </section>
-  )
+  );
 }
 
-export default Hero
+export default Hero;
